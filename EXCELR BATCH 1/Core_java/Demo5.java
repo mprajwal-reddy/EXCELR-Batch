416dd681888d80084679f5df575f266a6084a440
mprajwal-reddy <mprajwalreddy1216@gmail.com>
2025-01-10 12:47:31 +0530

    Add files via upload

---
+++ EXCELR BATCH 1/Core_java/Demo5.java
@@ -0,0 +1,13 @@
+package Core_java;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        String Username = "user001";
+        String Password = "user001";
+        while (("user001".equals(Username)) && ("user001".equals(Password))) {
+            System.out.println("Hello  Happy New Year");
+            break;
+        }
+    }
+
+}
---
+++ EXCELR BATCH 1/Core_java/Demo7.java
@@ -0,0 +1,13 @@
+package Core_java;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        String Name = "Prajwal";
+        int Age = 20;
+        long PhNO = 1234567890;
+        while (("Prajwal".equals(Name)) && (Age == 20) && (PhNO == 1234567890)) {
+            break;
+        }
+    }
+}
+
